7fe450b80ade8fa517f1ab0896709670e0a16fe2
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-10-26 15:03:14 -0400

    barebones homepage and login

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_PASSWORD,
     {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
     }
   );
 }
