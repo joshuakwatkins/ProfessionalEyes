d28a818f6c2163f1767de1e44205048ecd921872
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-10-28 13:06:21 -0400

    cleaned up javascript renamed project to draft

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,13 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const name = document.querySelector('#draft-name').value.trim();
+    const draftInput = document.querySelector('#draft-input').value.trim();
   
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
+    if (name && draftInput) {
+      const response = await fetch(`/api/drafts`, {
         method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
+        body: JSON.stringify({ name, draftInput }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -17,7 +16,7 @@ const newFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to create project');
+        alert('Failed to create draft');
       }
     }
   };
@@ -26,22 +25,22 @@ const newFormHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/projects/${id}`, {
+      const response = await fetch(`/api/drafts/${id}`, {
         method: 'DELETE',
       });
   
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to delete project');
+        alert('Failed to delete draft');
       }
     }
   };
   
   document
-    .querySelector('.new-project-form')
+    .querySelector('.new-draft-form')
     .addEventListener('submit', newFormHandler);
   
   document
-    .querySelector('.project-list')
+    .querySelector('.draft-list')
     .addEventListener('click', delButtonHandler);
