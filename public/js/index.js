794818a6fe47b4a40c7e68508bc25503de434d98
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-10-28 17:19:06 -0400

    fixed button

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 function copyMe() {
     /* Get the text field */
-    const copyText = document.getElementById("myInput");
+    const copyText = document.getElementById('copyEmail');
   
     /* Select the text field */
     copyText.select();
