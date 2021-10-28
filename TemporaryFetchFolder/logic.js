5c141120261f9800d32c3294cbf213937ff7812a
courtney long <courtneylong877@gmail.com>
2021-10-28 13:38:55 -0400

    fetch call to replace all negative words with synonyms

--- TemporaryFetchFolder/logic.js
+++ TemporaryFetchFolder/logic.js
@@ -9,6 +9,10 @@ console.log(partialJoin);
 
 // Fetch to retrieve a sentiment value for negative and positive words.
 
+// const negSentiment = [];
+
+// const getSentiment = (data) => {
+
 // fetch("https://twinword-sentiment-analysis.p.rapidapi.com/analyze/?text="+partialJoin, {
 // 	"method": "GET",
 // 	"headers": {
@@ -17,9 +21,16 @@ console.log(partialJoin);
 // 	}
 // })
 // .then(response => response.json())
-// .then(data => console.log(data))
-// ;
+// .then(data => getNegWords(data))
 
+// function getNegWords (data) { 
+// for (var i=0; i<data.keywords.length; i++) {
+//     if (data.keywords[i].score <= 0) {
+//         negSentiment.push(data.keywords[i].word)
+// }}};
+// }
+// getSentiment()
+// console.log(negSentiment)
 // Fetch to put astrics over any profane words.
 
 // fetch("https://community-purgomalum.p.rapidapi.com/json?text="+partialJoin, {
@@ -46,35 +57,35 @@ console.log(partialJoin);
 
 // Function to detect if any words are in all caps.
 
-const atLeastOneCap = [];
+// const atLeastOneCap = [];
 
-const isUpper = (str) => {
-    (/[A-Z]/.test(str))
-    if (/[A-Z]/.test(str)=== true) {
-        atLeastOneCap.push(str)
-        console.log(atLeastOneCap) 
-    }
-};
+// const isUpper = (str) => {
+//     (/[A-Z]/.test(str))
+//     if (/[A-Z]/.test(str)=== true) {
+//         atLeastOneCap.push(str)
+//         console.log(atLeastOneCap) 
+//     }
+// };
 
-textArray.forEach(element => isUpper(element));
+// textArray.forEach(element => isUpper(element));
 
-// Filter through the words with CAPS to pick out any with more than one capital letter.
+// // Filter through the words with CAPS to pick out any with more than one capital letter.
 
-const moreThanOneCap = [];
+// const moreThanOneCap = [];
 
-const isAllUpper = (str) => {
-    var howMany = (str.match(/[A-Z]/g) || []).length
-    if (howMany >= 2) {
-        moreThanOneCap.push(str)
-    }
-}
+// const isAllUpper = (str) => {
+//     var howMany = (str.match(/[A-Z]/g) || []).length
+//     if (howMany >= 2) {
+//         moreThanOneCap.push(str)
+//     }
+// }
 
-atLeastOneCap.forEach(element => isAllUpper(element));
+// atLeastOneCap.forEach(element => isAllUpper(element));
 
-console.log(moreThanOneCap)
-// Asking user if they want to change to lower case.
+// console.log(moreThanOneCap)
+// // Asking user if they want to change to lower case.
 
-const theCheckboxes = document.querySelector("#upperToLowerCase");
+// const theCheckboxes = document.querySelector("#upperToLowerCase");
 
 //Commented out for now so I can work with Griffin to push somewhere with bootstrap.
 
@@ -97,10 +108,74 @@ const theCheckboxes = document.querySelector("#upperToLowerCase");
     
     
 // }        
-if (moreThanOneCap.length >= 1) {
-    upperCaseToLowerCase();
-}
+// if (moreThanOneCap.length >= 1) {
+//     upperCaseToLowerCase();
+// }
 
 
 // Will need to show what words are true vs false for uppercase
 // Will need to pick out words that are caps only if greater than one letter (to ignore "I")
+
+
+// To supply the user with a positive twist on their email.
+
+// Require the library to run sentence analysis.
+
+// var WordPOS = require('wordpos'),
+// wordpos = new WordPOS();
+
+// wordpos.getAdjectives(text, function(result){
+//     console.log(result);
+// });
+
+// wordpos.getNouns(text, function(result){
+//     console.log(result);
+// });
+
+// wordpos.getVerbs(text, function(result){
+//     console.log(result);
+// });
+
+// wordpos.getAdverbs(text, function(result){
+//     console.log(result);
+// });
+
+// Testing negative word for synonym fetches.
+
+const fakeText = ["I think you're bad and I want to hit you.  I hope you fail and die."]
+
+const negTest = ["bad", "hit", "die", "fail"];
+const negFetchURL = [];
+// Set the API keys
+
+const thesKey = "0d6cde76-d8a1-48cd-9a08-41859be0f16f";
+const dictKey = "2868780d-1fe2-4f59-b567-ecb155bae7d1";
+
+// Make the thesaurus fetch call.
+
+const thesaurusCall = (element) => {
+fetch(element)
+.then(response => response.json())
+.then(data => switchWords(data))
+}
+negTest.forEach((element) => negFetchURL.push("https://www.dictionaryapi.com/api/v3/references/thesaurus/json/" + element +"?key=" + thesKey));
+negFetchURL.forEach((element) => thesaurusCall(element))
+
+
+
+const wordsWeWillSwitch = [];
+const switchWords = (data) => {
+    const randomArray = Math.floor(Math.random()*data[0].meta.syns.length);
+    const randomWord = Math.floor(Math.random()*data[0].meta.syns[randomArray].length);
+    const switchingWords = 
+    {
+        hwi: data[0].hwi.hw,
+        synonym: data[0].meta.syns[randomArray][randomWord]
+    }
+    wordsWeWillSwitch.push(switchingWords);
+}
+
+console.log(wordsWeWillSwitch);
+
+// Pick a synonym out to replace a bad word with.
+
