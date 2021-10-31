020dd21dc545cd8d4192efd49ffa4506a2c55b09
Josh Watkins <joshwatkinssfs@gmail.com>
2021-10-31 16:03:03 -0400

    sorting the env data

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,17 @@ require("dotenv").config();
 
 let sequelize;
 
-DB_NAME = "cleancomm_db"
-DB_USER = "root"
-DB_PASSWORD = "rootPW1"
+// DB_NAME = "cleancomm_db"
+// DB_USER = "root"
+// DB_PASSWORD = "rootPW1"
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    DB_NAME,
-    DB_USER,
-    DB_PASSWORD,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: "localhost",
       dialect: "mysql",
